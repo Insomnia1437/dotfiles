eb91714d11a5822d437e0a913fc1efb8d90dfeba
Insomnia1437 <sdcswd@gmail.com>
2020-06-04 18:05:20 +0900

    update

--- custom/common.sh
+++ custom/common.sh
@@ -2,14 +2,14 @@
 # setenv PATH /usr/new/pkg/git/current/bin:$PATH
 
 export LANG=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 export TERM="xterm-256color"
 
 export VISUAL=vim
 export EDITOR=vim
 
 # add node and npm to path
-export PATH=/usr/new/pkg/node/current_x64/bin/:$PATH
+export PATH=/usr/new/pkg/node/11.7.0_c6_x64/bin/:$PATH
 
 # https://superuser.com/questions/1195962/cannot-make-directory-var-run-screen-permission-denied
 if [[ ! -e /var/run/screen && ! -e /run/screen && ! -d ~/.screen ]];then
@@ -17,14 +17,3 @@ if [[ ! -e /var/run/screen && ! -e /run/screen && ! -d ~/.screen ]];then
     chmod 700 ~/.screen
     export SCREENDIR=~/.screen
 fi
-
-# set ssh Server
-# alias   vera='ssh -X sdcswd@vera'
-# alias   ganeza='ssh -X sdcswd@ganeza'
-# alias   ganapati='ssh -X sdcswd@ganapati'
-# alias   wang='ssh -X sdcswd@wang'
-# alias   abcob06='ssh -X sdcswd@abcob06'
-# alias   abco7='ssh sdcswd@abco7'
-
-# for elasticsearch
-# export ES_HEAP_SIZE=6g

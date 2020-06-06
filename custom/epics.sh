d7c85efd0eb55caee5d31acf740798f3faab3fdb
Insomnia1437 <sdcswd@gmail.com>
2020-06-06 22:40:56 +0900

    update

--- custom/epics.sh
+++ custom/epics.sh
@@ -28,13 +28,14 @@ export  EPICS_EXTENSIONS="~/epics/R3.14.12.8/extensions"
 export  EPICS_CA_MAX_ARRAY_BYTES=10000000
 # export EPICS_CA_AUTO_ADDR_LIST NO
 # export EPICS_CA_ADDR_LIST='172.19.64.78'
-export  PATH=$EPICS_BASE/bin/$EPICS_HOST_ARCH:$PATH
-export  PATH=${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}:$PATH
+# export  PATH=$EPICS_BASE/bin/$EPICS_HOST_ARCH:$PATH
+# export  PATH=${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}:$PATH
 
 export  LD_LIBRARY_PATH=$EPICS_BASE/lib/$EPICS_HOST_ARCH
 
 alias cdb="cd ${EPICS_BASE}"
 alias cde="cd ${EPICS_EXTENSIONS}"
 alias cdi="cd ${EPICS_BASE}/../ioc"
+alias cdm="cd ${EPICS_BASE}/../modules"
 alias cdd="cd ~/epics/download"
 alias cdp="cd ~/workspace/python"

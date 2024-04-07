b067247afdcdbe4a6ffbc6fe1e46acb16df9c27e
DW <sdcswd@gmail.com>
2024-04-07 10:23:42 +0200

    fix git depth=0 error

--- scripts/git_install.sh
+++ scripts/git_install.sh
@@ -3,11 +3,16 @@
 # Set git depth to 5 so that I can reset the history and manually merge
 # Old git version may get error
 # fatal: git fetch-pack: expected shallow list
-# So we need to set depth to 0 sometimes
-GIT_DEPTH=$1
+# So we need to disable shallow clone depth to 0 sometimes
+if [ $1 == "0" ];then
+  GIT_DEPTH_OPTION=""
+else
+  GIT_DEPTH_OPTION="--depth=$1"
+fi
+
 if [ ! -d "$3" ]; then
 #   git clone --recurse-submodules --depth=1 "$2" "$3"
-  git clone --depth=$GIT_DEPTH "$2" "$3"
+  git clone $GIT_DEPTH_OPTION "$2" "$3"
 else
-  cd "$3" && git pull --ff-only --depth=$GIT_DEPTH
+  cd "$3" && git pull --ff-only $GIT_DEPTH_OPTION
 fi

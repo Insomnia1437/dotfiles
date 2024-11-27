7a6c19503855871916d1ff6b22173f0e84e0764e
DW <sdcswd@gmail.com>
2024-11-27 16:28:24 +0900

    support epics of different path

--- dotfiles/common/epics.sh
+++ dotfiles/common/epics.sh
@@ -1,11 +1,16 @@
 # change EPICS
 # match EPICS version
+if [[ -n $2 ]]; then
+    EPICS_ROOT=$2
+else
+    EPICS_ROOT=${HOME}
+fi
 old_epics_version=${EPICS_VERSION}
 if [[ -n old_epics_version ]]; then
 # to match the colon. Should colon be escaped?
-    old_path1="${HOME}/epics/${old_epics_version}/base/bin/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
-    old_path2="${HOME}/epics/${old_epics_version}/extensions/bin/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
-    old_path3="${HOME}/epics/${old_epics_version}/base/lib/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
+    old_path1="${EPICS_ROOT}/epics/${old_epics_version}/base/bin/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
+    old_path2="${EPICS_ROOT}/epics/${old_epics_version}/extensions/bin/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
+    old_path3="${EPICS_ROOT}/epics/${old_epics_version}/base/lib/${EPICS_HOST_ARCH}\(:\)\{0,1\}"
     # remove old path for epics base and extesion
     # use "#" for sed to avoid escape of "/"
     export  PATH=$(echo ${PATH} | sed "s#${old_path1}##g")
@@ -23,8 +28,8 @@ else
     EPICS_VERSION="R7.0.8"
 fi
 export  EPICS_VERSION="${EPICS_VERSION}"
-export  EPICS_BASE="${HOME}/epics/${EPICS_VERSION}/base"
-export  EPICS_EXTENSIONS="${HOME}/epics/${EPICS_VERSION}/extensions"
+export  EPICS_BASE="${EPICS_ROOT}/epics/${EPICS_VERSION}/base"
+export  EPICS_EXTENSIONS="${EPICS_ROOT}/epics/${EPICS_VERSION}/extensions"
 export  EPICS_CA_MAX_ARRAY_BYTES="10000000"
 export  PATH="${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:${PATH}"
 export  PATH="${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}:${PATH}"
@@ -33,9 +38,9 @@ export  PATH="${EPICS_EXTENSIONS}/bin/${EPICS_HOST_ARCH}:${PATH}"
 
 alias cdb="cd ${EPICS_BASE}"
 alias cde="cd ${EPICS_EXTENSIONS}"
-alias cdi="cd ${HOME}/epics/${EPICS_VERSION}/ioc"
-alias cdm="cd ${HOME}/epics/${EPICS_VERSION}/modules"
-alias cded="cd ${HOME}/epics/downloads"
+alias cdi="cd ${EPICS_ROOT}/epics/${EPICS_VERSION}/ioc"
+alias cdm="cd ${EPICS_ROOT}/epics/${EPICS_VERSION}/modules"
+alias cded="cd ${EPICS_ROOT}/epics/downloads"
 
 # disable epics iocsh history (epics version>=7.0.8)
 export EPICS_IOCSH_HISTFILE=""

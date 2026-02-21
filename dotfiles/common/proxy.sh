5ea6468e7bcd929c9b15b2d72b405addb57bc6e9
DW <sdcswd@gmail.com>
2026-02-22 08:42:22 +0900

    change proxy setting

--- dotfiles/common/proxy.sh
+++ dotfiles/common/proxy.sh
@@ -1,7 +1,27 @@
 # define proxy server if necessary
 {%@@ if USE_PROXY == "YES" @@%}
 # active proxy in ~/.config/common/local.sh
-alias setproxy="export http_proxy=http://{{@@ PROXY @@}}; export https_proxy=http://{{@@ PROXY @@}}; export HTTPS_PROXY=http://{{@@ PROXY @@}}; export HTTPS_PROXY=http://{{@@ PROXY @@}}; echo 'Proxy on'"
-alias noproxy="unset http_proxy; unset https_proxy; unset HTTP_PROXY; unset HTTPS_PROXY; echo 'Proxy off'"
+setproxy() {
+    export http_proxy="http://{{@@ PROXY @@}}"
+    export https_proxy="http://{{@@ PROXY @@}}"
+    export HTTP_PROXY="http://{{@@ PROXY @@}}"
+    export HTTPS_PROXY="http://{{@@ PROXY @@}}"
+    export ALL_PROXY="http://{{@@ PROXY @@}}"
+    export all_proxy="http://{{@@ PROXY @@}}"
+    export no_proxy="{{@@ NO_PROXY @@}}"
+    echo "Proxy on"
+}
+
+unsetproxy() {
+    unset http_proxy
+    unset https_proxy
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset ALL_PROXY
+    unset all_proxy
+    unset no_proxy
+    echo "Proxy off"
+}
+
 {%@@ endif @@%}
 

a6584a9fb52b53b60871a2133397f702a562a86a
DW <sdcswd@gmail.com>
2026-03-30 21:04:40 +0900

    update fzf config, remove ohmyzsh fzf plugin
    Signed-off-by: DW <sdcswd@gmail.com>

--- dotfiles/fzf/fzf.sh
+++ dotfiles/fzf/fzf.sh
@@ -1,29 +1,38 @@
-export FZF_DEFAULT_COMMAND='fd --type f \
+FZF_DEFAULT_COMMAND="fd --type f \
   --strip-cwd-prefix \
-  --follow \
   --exclude .git \
   --exclude node_modules \
   --exclude target \
   --exclude dist \
   --exclude build \
-  --max-depth 5'
-export FZF_DEFAULT_OPTS='
+  --max-depth 5"
+  # --follow \
+
+FZF_DEFAULT_OPTS="
   --height 40%
   --layout=reverse
   --border
-  --info=inline
-'
+  --info=inline"
 
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'bat --theme=GitHub --style=numbers --color=always --line-range :500 {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+FZF_CTRL_R_OPTS="
+  --prompt='History> '
+  --color header:italic
+  --header 'Search through command history with fzf.'"
+  # --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+
+FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+FZF_CTRL_T_OPTS="
+  --preview 'command -v bat >/dev/null && bat --theme=GitHub --style=numbers --color=always --line-range :500 {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+"
+  # --walker-skip .git,node_modules,target,dist,build
 
 
-export FZF_ALT_C_COMMAND='fd --type d --hidden --follow \
+FZF_ALT_C_COMMAND="fd --type d --hidden --follow \
   --exclude .git \
   --exclude node_modules \
-  --max-depth 5'
-export FZF_ALT_C_OPTS='
-'
+  --max-depth 5"
+
+FZF_ALT_C_OPTS="
+  --preview 'command -v tree >/dev/null && tree -C {} | head -200'
+"
